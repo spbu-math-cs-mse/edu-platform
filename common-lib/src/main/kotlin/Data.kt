36c80159dba80234742ed3239bf73b2cf3d0532c
metametamoon <sorvinlevs@gmail.com>
2024-10-24 14:59:50 +0300

    build(api): created a general interface for the system

---
+++ common-lib/src/main/kotlin/Data.kt
@@ -0,0 +1,46 @@
+@Suppress("unused") // since it's a public api
+
+data class Student(val id: String)
+data class Teacher(val id: String)
+data class Problem(val id: String)
+data class Solution(val id: String, val problem: Problem)
+typealias Grade = Int
+
+class Course(
+    val teachers: MutableList<Teacher>,
+    val students: MutableList<Student>,
+    var description: String,
+)
+
+
+data class SolutionContent(
+    val text: String // probably not just text in general
+)
+
+data class SolutionAssessment(
+    val grade: Grade, val comments: String
+)
+
+interface GradeTable {
+    fun addAssessment(
+        student: Student,
+        teacher: Teacher,
+        solution: Solution,
+        assessment: SolutionAssessment,
+    )
+
+    fun getGradeMap(): Map<Student, Map<Problem, Grade>>
+}
+
+interface SolutionDistributor {
+    fun inputSolution(
+        student: Student,
+        solutionContent: SolutionContent
+    ): Solution
+
+    fun querySolution(teacher: Teacher): Pair<Solution, SolutionContent>
+    fun assessSolution(
+        solution: Solution,
+        teacher: Teacher, assessment: SolutionAssessment, gradeTable: GradeTable
+    )
+}

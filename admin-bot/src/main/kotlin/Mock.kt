231ab2b341025c378f15e62c2b359a83cd3bfb49
annapiatkova <144122088+annapiatkova@users.noreply.github.com>
2024-10-29 10:46:28 +0300

    spotless

--- admin-bot/src/main/kotlin/Mock.kt
+++ admin-bot/src/main/kotlin/Mock.kt
@@ -8,24 +8,26 @@ import Teacher
 val mockGradeTable = MockGradeTable()
 
 val mockCourses: MutableMap<String, Course> = mutableMapOf(
-    "Геома 1" to Course(mutableListOf(Teacher("1")), mutableListOf(Student("1")),
-        "какое-то описание", mockGradeTable)
+  "Геома 1" to Course(
+    mutableListOf(Teacher("1")), mutableListOf(Student("1")),
+    "какое-то описание", mockGradeTable,
+  ),
 )
 
 var mockTgUsername: String = ""
 
 val mockStudents: MutableMap<String, String> = mutableMapOf(
-    "1" to "@username_of_a_student",
-    "2" to "@another_username"
+  "1" to "@username_of_a_student",
+  "2" to "@another_username",
 )
 
 val mockTeachers: MutableMap<String, String> = mutableMapOf(
-    "1" to "@username_of_a_teacher",
-    "2" to "@some_username"
+  "1" to "@username_of_a_teacher",
+  "2" to "@some_username",
 )
 
 val mockAdmins: MutableMap<String, String> by lazy {
-    mutableMapOf(
-        "1" to mockTgUsername
-    )
+  mutableMapOf(
+    "1" to mockTgUsername,
+  )
 }

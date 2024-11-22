ff1224db7e992a12374ef498587eececa9c0bd76
Nosorozhek <nosorozhek239@gmail.com>
2024-11-23 00:19:11 +0300

    Fixed test

--- student-bot/src/test/kotlin/StudentBotTest.kt
+++ student-bot/src/test/kotlin/StudentBotTest.kt
@@ -11,12 +11,13 @@ class StudentBotTest {
     val mockCoursesDistributor = MockCoursesDistributor()
     val userId = mockCoursesDistributor.singleUserId
     val mockSolutionDistributor = MockSolutionDistributor()
-    val studentCore = StudentCore(
-      mockSolutionDistributor,
-      mockCoursesDistributor,
-    )
+    val studentCore =
+      StudentCore(
+        mockSolutionDistributor,
+        mockCoursesDistributor,
+      )
     run {
-      val firstCourse = studentCore.getAvailableCourses(userId).first()
+      val firstCourse = studentCore.getStudentCourses(userId).first()
       val firstAssignment = firstCourse.assignments.first()
       (firstCourse.gradeTable as MockGradeTable).addMockFilling(
         firstAssignment,
@@ -24,15 +25,16 @@ class StudentBotTest {
       )
     }
     // check first input correctness
-    val availableCourses = studentCore.getAvailableCourses(userId)
+    val availableCourses = studentCore.getStudentCourses(userId)
     assert(availableCourses.any { it.id == "0" })
     val course = mockCoursesDistributor.getCourses("0").first()
     val assignment = course.assignments.first()
-    val grading = studentCore.getGradingForAssignment(
-      assignment,
-      course,
-      userId,
-    )
+    val grading =
+      studentCore.getGradingForAssignment(
+        assignment,
+        course,
+        userId,
+      )
     // check output correctness
     assertEquals(listOf(null, 1, null, 0), grading.map { (_, grade) -> grade })
   }

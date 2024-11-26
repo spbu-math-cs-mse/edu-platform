b701971fd9a09e1148c6a78dc66aa3ae6a4bad3a
metametamoon <sorvinlevs@gmail.com>
2024-11-26 22:22:03 +0300

    wip: implemented without testing the database course distributor
    # Conflicts:
    #	common-lib/src/main/kotlin/CoursesDistributor.kt

--- common-lib/src/main/kotlin/database/DatabaseCoursesDistributor.kt
+++ common-lib/src/main/kotlin/database/DatabaseCoursesDistributor.kt
@@ -1,8 +1,10 @@
-import com.github.heheteam.commonlib.Course
-import com.github.heheteam.commonlib.CoursesDistributor
-import com.github.heheteam.commonlib.database.tables.CourseStudents
+import com.github.heheteam.commonlib.*
+import com.github.heheteam.commonlib.database.tables.AssignmentTable
 import com.github.heheteam.commonlib.database.tables.CourseTable
+import com.github.heheteam.commonlib.database.tables.CourseStudents
+import com.github.heheteam.commonlib.database.tables.ProblemTable
 import com.github.heheteam.commonlib.database.toIntIdHack
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
@@ -11,7 +13,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 class DatabaseCoursesDistributor(val database: Database) : CoursesDistributor {
   override fun addRecord(studentId: String, courseId: String) {
     transaction(database) {
-      val id = CourseStudents.insert {
+      CourseStudents.insert {
         it[CourseStudents.studentId] = studentId.toIntIdHack()
         it[CourseStudents.courseId] = courseId.toIntIdHack()
       }
@@ -19,22 +21,66 @@ class DatabaseCoursesDistributor(val database: Database) : CoursesDistributor {
   }
 
   override fun getCoursesBulletList(studentId: String): String {
-    TODO("Not yet implemented")
+    return getCourses(studentId).joinToString("\n") { course: Course -> "-" + course.description }
   }
 
   override fun getCourses(studentId: String): List<Course> {
-    val courseRows = transaction {
+    val courses = transaction {
       val courseIds = CourseStudents.selectAll()
         .where { CourseStudents.studentId eq studentId.toIntIdHack() }
         .map { it[CourseStudents.courseId] }
-      val courseRows = courseIds.map { courseId ->
-        CourseTable.selectAll().where { CourseTable.id eq courseId }.single()
+      val courses = courseIds.map { courseId ->
+        val courseRow =
+          CourseTable.selectAll().where { CourseTable.id eq courseId }.single()
+        val courseDescription = courseRow[CourseTable.description]
+        val preassignments = AssignmentTable
+          .selectAll()
+          .where { AssignmentTable.course eq courseId }
+          .map { it[AssignmentTable.id] to it[AssignmentTable.description] }
+          .toList()
+        val assignments =
+          preassignments.map { (assignmentId, assignmentDescription) ->
+            queryAssignment(assignmentId, assignmentDescription, courseId)
+          }
+        Course(
+          courseId.toString(),
+          mutableListOf(),
+          mutableListOf(),
+          courseDescription,
+          MockGradeTable(),
+          assignments.toMutableList()
+        )
       }
+      courses
     }
-    TODO("Not yet implemented")
+    return courses
   }
 
-  override fun getAvailableCourses(studentId: String): MutableList<Pair<Course, Boolean>> {
+  private fun queryAssignment(
+    assignmentId: EntityID<Int>,
+    assignmentDescription: String,
+    courseId: EntityID<Int>
+  ): Assignment {
+    val problems = ProblemTable.selectAll()
+      .where { ProblemTable.assignment eq assignmentId }
+      .map {
+        Problem(
+          it[ProblemTable.id].toString(),
+          it[ProblemTable.id].toString(),
+          it[ProblemTable.description],
+          it[ProblemTable.maxScore],
+          assignmentId.toString()
+        )
+      }
+    return Assignment(
+      assignmentId.toString(),
+      assignmentDescription,
+      problems.toMutableList(),
+      courseId.toString()
+    )
+  }
+
+  override fun getAvailableCourses(studentId: String): List<Pair<Course, Boolean>> {
     TODO("Not yet implemented")
   }
 }

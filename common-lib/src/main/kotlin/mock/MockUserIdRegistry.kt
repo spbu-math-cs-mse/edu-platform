1214de73015672bb2b4b50139f1db25542f1fd27
RSMT98 <robsmt@mail.ru>
2024-12-04 05:24:27 +0300

    fixed mockIdRegistry

--- common-lib/src/main/kotlin/mock/MockUserIdRegistry.kt
+++ common-lib/src/main/kotlin/mock/MockUserIdRegistry.kt
@@ -6,23 +6,23 @@ import dev.inmo.tgbotapi.types.UserId
 class MockAdminIdRegistry(
   val usedId: Long,
 ) : AdminIdRegistry {
-  override fun getUserId(tgId: UserId): AdminId = AdminId(usedId)
+  override fun getUserId(tgId: UserId): AdminId? = if (tgId.chatId.long == usedId) AdminId(usedId) else null
 }
 
 class MockStudentIdRegistry(
   val usedId: Long,
 ) : StudentIdRegistry {
-  override fun getUserId(tgId: UserId): StudentId = StudentId(usedId)
+  override fun getUserId(tgId: UserId): StudentId? = if (tgId.chatId.long == usedId) StudentId(usedId) else null
 }
 
 class MockTeacherIdRegistry(
   val usedId: Long,
 ) : TeacherIdRegistry {
-  override fun getUserId(tgId: UserId): TeacherId = TeacherId(usedId)
+  override fun getUserId(tgId: UserId): TeacherId? = if (tgId.chatId.long == usedId) TeacherId(usedId) else null
 }
 
 class MockParentIdRegistry(
   val usedId: Long,
 ) : ParentIdRegistry {
-  override fun getUserId(tgId: UserId): ParentId = ParentId(usedId)
+  override fun getUserId(tgId: UserId): ParentId? = if (tgId.chatId.long == usedId) ParentId(usedId) else null
 }

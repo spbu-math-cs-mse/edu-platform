903712022e2101a852be45ffd13c9e123ce9a790
RSMT98 <robsmt@mail.ru>
2024-12-04 07:54:57 +0300

    Revert "fixed mockIdRegistry"
    This reverts commit 1214de73015672bb2b4b50139f1db25542f1fd27.

--- common-lib/src/main/kotlin/mock/MockUserIdRegistry.kt
+++ common-lib/src/main/kotlin/mock/MockUserIdRegistry.kt
@@ -6,23 +6,23 @@ import dev.inmo.tgbotapi.types.UserId
 class MockAdminIdRegistry(
   val usedId: Long,
 ) : AdminIdRegistry {
-  override fun getUserId(tgId: UserId): AdminId? = if (tgId.chatId.long == usedId) AdminId(usedId) else null
+  override fun getUserId(tgId: UserId): AdminId = AdminId(usedId)
 }
 
 class MockStudentIdRegistry(
   val usedId: Long,
 ) : StudentIdRegistry {
-  override fun getUserId(tgId: UserId): StudentId? = if (tgId.chatId.long == usedId) StudentId(usedId) else null
+  override fun getUserId(tgId: UserId): StudentId = StudentId(usedId)
 }
 
 class MockTeacherIdRegistry(
   val usedId: Long,
 ) : TeacherIdRegistry {
-  override fun getUserId(tgId: UserId): TeacherId? = if (tgId.chatId.long == usedId) TeacherId(usedId) else null
+  override fun getUserId(tgId: UserId): TeacherId = TeacherId(usedId)
 }
 
 class MockParentIdRegistry(
   val usedId: Long,
 ) : ParentIdRegistry {
-  override fun getUserId(tgId: UserId): ParentId? = if (tgId.chatId.long == usedId) ParentId(usedId) else null
+  override fun getUserId(tgId: UserId): ParentId = ParentId(usedId)
 }

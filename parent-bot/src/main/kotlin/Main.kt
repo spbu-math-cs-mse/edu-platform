d2ef3d446af5b0640286b2467b3dfd212e1a2425
Dmitrii-01 <144121834+Dmitrii-01@users.noreply.github.com>
2024-12-05 14:47:13 +0300

    removing duplicated code

--- parent-bot/src/main/kotlin/Main.kt
+++ parent-bot/src/main/kotlin/Main.kt
@@ -4,23 +4,8 @@ import com.github.heheteam.commonlib.database.DatabaseGradeTable
 import com.github.heheteam.commonlib.database.DatabaseSolutionDistributor
 import com.github.heheteam.commonlib.database.DatabaseStudentStorage
 import com.github.heheteam.commonlib.mock.*
-import com.github.heheteam.parentbot.states.StartState
-import com.github.heheteam.parentbot.states.strictlyOnChildPerformanceState
-import com.github.heheteam.parentbot.states.strictlyOnGivingFeedbackState
-import com.github.heheteam.parentbot.states.strictlyOnMenuState
-import com.github.heheteam.parentbot.states.strictlyOnStartState
-import dev.inmo.kslog.common.KSLog
-import dev.inmo.kslog.common.LogLevel
-import dev.inmo.kslog.common.defaultMessageFormatter
-import dev.inmo.micro_utils.coroutines.subscribeSafelyWithoutExceptions
-import dev.inmo.tgbotapi.bot.ktor.telegramBot
-import dev.inmo.tgbotapi.extensions.api.bot.getMe
-import dev.inmo.tgbotapi.extensions.behaviour_builder.telegramBotWithBehaviourAndFSMAndStartLongPolling
-import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.command
-import dev.inmo.tgbotapi.extensions.utils.extensions.raw.from
+import com.github.heheteam.parentbot.run.parentRun
 import dev.inmo.tgbotapi.utils.RiskFeature
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 
 /**
@@ -29,50 +14,21 @@ import org.jetbrains.exposed.sql.Database
 @OptIn(RiskFeature::class)
 suspend fun main(vararg args: String) {
   val botToken = args.first()
-  telegramBot(botToken) {
-    logger =
-      KSLog { level: LogLevel, tag: String?, message: Any, throwable: Throwable? ->
-        println(defaultMessageFormatter(level, tag, message, throwable))
-      }
-  }
 
-  telegramBotWithBehaviourAndFSMAndStartLongPolling(
-    botToken,
-    CoroutineScope(Dispatchers.IO),
-    onStateHandlingErrorHandler = { state, e ->
-      println("Thrown error on $state")
-      e.printStackTrace()
-      state
-    },
-  ) {
-    println(getMe())
+  val database =
+    Database.connect(
+      "jdbc:h2:./data/films",
+      driver = "org.h2.Driver",
+    )
 
-    command("start") {
-      if (it.from != null) {
-        startChain(StartState(it.from!!))
-      }
-    }
-    val database =
-      Database.connect(
-        "jdbc:h2:./data/films",
-        driver = "org.h2.Driver",
-      )
+  val userIdRegistry = MockParentIdRegistry(1)
 
-    val userIdRegistry = MockParentIdRegistry(1)
-    val core =
-      ParentCore(
-        DatabaseStudentStorage(database),
-        DatabaseGradeTable(database),
-        DatabaseSolutionDistributor(database),
-      )
+  val core =
+    ParentCore(
+      DatabaseStudentStorage(database),
+      DatabaseGradeTable(database),
+      DatabaseSolutionDistributor(database),
+    )
 
-    strictlyOnStartState(isDeveloperRun = true)
-    strictlyOnMenuState(userIdRegistry, core)
-    strictlyOnGivingFeedbackState(userIdRegistry)
-    strictlyOnChildPerformanceState(userIdRegistry, core)
-
-    allUpdatesFlow.subscribeSafelyWithoutExceptions(this) {
-      println(it)
-    }
-  }.second.join()
+  parentRun(botToken, userIdRegistry, core)
 }

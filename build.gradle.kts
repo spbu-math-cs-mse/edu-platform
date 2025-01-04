471ab325b6a89ac9659c04658c716a04d5e417b5
metametamoon <sorvinlevs@gmail.com>
2025-01-04 22:56:47 +0300

    fix: properly configured detekt for all subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,14 @@ allprojects {
 
     apply(plugin = "kotlin")
     apply(plugin = "kotlinx-serialization")
-
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
+        buildUponDefaultConfig = true
+        baseline = file("$rootDir/config/detekt/baseline.xml")
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        parallel = false
+        ignoreFailures = false
+    }
     java {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(21))
@@ -32,16 +39,12 @@ allprojects {
         testImplementation(kotlin("test"))
         implementation("redis.clients:jedis:5.1.0")
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.7")
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-libraries:1.23.7")
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-ruleauthors:1.23.7")
     }
 }
 
-detekt {
-    buildUponDefaultConfig = true
-    baseline = file("$rootDir/config/detekt/baseline.xml")
-    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
-    parallel = false
-    ignoreFailures = false
-}
 
 val configFile = files("$rootDir/config/detekt/detekt.yml")
 val baselineFile = file("$rootDir/config/detekt/baseline.xml")
@@ -68,8 +71,9 @@ tasks.register<io.gitlab.arturbosch.detekt.Detekt>("detektAll") {
     autoCorrect = false
     buildUponDefaultConfig = true
     setSource(projectDir)
-    if (baselineFile.exists())
+    if (baselineFile.exists()) {
         baseline.set(file(baselineFile))
+    }
     config.setFrom(files(configFile))
     include(kotlinFiles)
     exclude(resourceFiles, buildFiles)
@@ -79,8 +83,9 @@ tasks.register<io.gitlab.arturbosch.detekt.Detekt>("detektAll") {
     }
 }
 
-dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.7")
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-libraries:1.23.7")
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-ruleauthors:1.23.7")
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        md.required.set(true)
+    }
 }

14bea7ee6608f718b126aabac95d8a57d215e129
Nosorozhek <nosorozhek239@gmail.com>
2025-05-27 20:36:27 +0300

    fix: revert test fix

--- common-lib/src/main/kotlin/com/github/heheteam/commonlib/database/Init.kt
+++ common-lib/src/main/kotlin/com/github/heheteam/commonlib/database/Init.kt
@@ -57,7 +57,7 @@ fun main() {
 
 fun reset(database: Database) {
   transaction(database) {
-    exec(allTables.joinToString("\n") { "DROP TABLE IF EXISTS \"${it.tableName}\" CASCADE;" })
+    exec("DROP TABLE IF EXISTS ${allTables.joinToString(", ") { it.tableName }} CASCADE;")
     create(*allTables)
   }
 }

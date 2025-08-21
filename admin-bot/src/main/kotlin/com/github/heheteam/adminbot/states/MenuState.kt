ad5ac779888b18c218e7a1e6005a8308450bb632
Nosorozhek <nosorozhek239@gmail.com>
2025-08-21 18:48:34 +0300

    fix: fixed registration of the scheduled messages states

--- admin-bot/src/main/kotlin/com/github/heheteam/adminbot/states/MenuState.kt
+++ admin-bot/src/main/kotlin/com/github/heheteam/adminbot/states/MenuState.kt
@@ -9,7 +9,7 @@ import com.github.heheteam.adminbot.AdminKeyboards.EDIT_COURSE
 import com.github.heheteam.adminbot.AdminKeyboards.SEND_SCHEDULED
 import com.github.heheteam.adminbot.Dialogues
 import com.github.heheteam.adminbot.states.general.AdminBotStateWithHandlers
-import com.github.heheteam.adminbot.states.scheduled.ScheduledMessagesMenu
+import com.github.heheteam.adminbot.states.scheduled.ScheduledMessagesMenuState
 import com.github.heheteam.commonlib.api.AdminApi
 import com.github.heheteam.commonlib.errors.FrontendError
 import com.github.heheteam.commonlib.interfaces.AdminId
@@ -68,7 +68,7 @@ class MenuState(override val context: User, val adminId: AdminId) :
           } ?: Unhandled
         }
         ADD_ADMIN -> NewState(AddAdminState(context, adminId))
-        SEND_SCHEDULED -> NewState(ScheduledMessagesMenu(context, adminId))
+        SEND_SCHEDULED -> NewState(ScheduledMessagesMenuState(context, adminId))
         else -> Unhandled
       }
     }

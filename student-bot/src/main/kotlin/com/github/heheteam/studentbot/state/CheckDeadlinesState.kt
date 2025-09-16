cb2c7862e41515568417252555cd2aa88b6b2187
metametamoon <sorvinlevs@gmail.com>
2025-09-16 17:52:27 +0300

    fix: detekt

--- student-bot/src/main/kotlin/com/github/heheteam/studentbot/state/CheckDeadlinesState.kt
+++ student-bot/src/main/kotlin/com/github/heheteam/studentbot/state/CheckDeadlinesState.kt
@@ -1,6 +1,8 @@
 package com.github.heheteam.studentbot.state
 
+import com.github.heheteam.commonlib.Assignment
 import com.github.heheteam.commonlib.Course
+import com.github.heheteam.commonlib.Problem
 import com.github.heheteam.commonlib.api.StudentApi
 import com.github.heheteam.commonlib.interfaces.StudentId
 import com.github.heheteam.commonlib.state.BotContext
@@ -13,6 +15,7 @@ import com.github.heheteam.commonlib.util.map
 import com.github.michaelbull.result.coroutines.coroutineBinding
 import dev.inmo.micro_utils.fsm.common.State
 import dev.inmo.tgbotapi.types.chat.User
+import dev.inmo.tgbotapi.types.message.textsources.TextSourcesList
 import dev.inmo.tgbotapi.types.message.textsources.bold
 import dev.inmo.tgbotapi.types.message.textsources.italic
 import dev.inmo.tgbotapi.types.message.textsources.regular
@@ -38,29 +41,33 @@ class CheckDeadlinesState(
           .getActiveProblems(userId, course.id)
           .bind()
           .filterByDeadlineAndSort(getCurrentMoscowTime())
-      val messageText =
-        if (problemsWithPersonalDeadlines.isEmpty()) {
-          buildEntities(" ") { +"Нет активных дедлайнов" }
-        } else {
-          buildEntities(" ") {
-            problemsWithPersonalDeadlines
-              .sortedBy { it.first.id.long }
-              .forEach { (assignment, problems) ->
-                +bold(assignment.name) + regular("\n")
-                service.calculateRescheduledDeadlines(userId, problems).forEach { problem ->
-                  val formattedDeadline =
-                    problem.deadline?.format(deadlineFormat)?.let { regular(it) }
-                  +" • ${problem.number}:   " + (formattedDeadline ?: italic("Без дедлайна")) + "\n"
-                }
-                +"\n"
-              }
-          }
-        }
+      val messageText = constructDeadlineMessage(problemsWithPersonalDeadlines, service)
       val okMenu = buildColumnMenu(ButtonData("Хорошо", "ok") { defaultState() }).map(::NewState)
       registerStateMenu(okMenu)
       send(messageText, okMenu.keyboard)
     }
   }
+
+  private fun constructDeadlineMessage(
+    problemsWithPersonalDeadlines: List<Pair<Assignment, List<Problem>>>,
+    service: StudentApi,
+  ): TextSourcesList =
+    if (problemsWithPersonalDeadlines.isEmpty()) {
+      buildEntities(" ") { +"Нет активных дедлайнов" }
+    } else {
+      buildEntities(" ") {
+        problemsWithPersonalDeadlines
+          .sortedBy { it.first.id.long }
+          .forEach { (assignment, problems) ->
+            +bold(assignment.name) + regular("\n")
+            service.calculateRescheduledDeadlines(userId, problems).forEach { problem ->
+              val formattedDeadline = problem.deadline?.format(deadlineFormat)?.let { regular(it) }
+              +" • ${problem.number}:   " + (formattedDeadline ?: italic("Без дедлайна")) + "\n"
+            }
+            +"\n"
+          }
+      }
+    }
 
   private val deadlineFormat =
     LocalDateTime.Format {
